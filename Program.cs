bd64115f87e2b13c565ca5fd859642026cd046b7
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-15 18:11:43 +0530

    Operations

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SortingTechniques
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //BubbleSort sort = new BubbleSort();
+            //sort.DisplayBubbleSort();
+            //PrimeNum prime= new PrimeNum();
+            //prime.Display();
+            //InsertionSort.DisplayInsertion();
+            Anagram.DisplayAnagram();
+        }
+    }
+}
