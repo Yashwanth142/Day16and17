4249c98b113ca1fd49a636dd7b4b20c44127e4ab
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-15 18:15:22 +0530

    Prime number added

---
+++ PrimeNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingTechniques
+{
+    public class PrimeNum
+    {
+        public void Display()
+        {
+            Console.WriteLine("The prime number form 1 to 1000 are :");
+            for (int num = 2; num <= 1000; num++)
+            {
+                int p = 0;
+                for (int i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        p = 1;
+                        break;
+                    }
+                }
+                if (p == 0)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+        }
+    }
+}
